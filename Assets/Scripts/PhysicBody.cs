a3756af519cbe7788eb7f916a9ef8b8d7d02d8b5
matthieu-blin <matth.blin@gmail.com>
2021-01-20 16:41:54 +0100

    Ajout de la deuxième loi de newton (approximation d'euler)

--- Assets/Scripts/PhysicBody.cs
+++ Assets/Scripts/PhysicBody.cs
@@ -6,9 +6,14 @@ public class PhysicBody : MonoBehaviour
 {
     [SerializeField]
     Vector3 m_velocity = Vector3.zero;
+    [SerializeField]
+    Vector3 m_force = Vector3.zero;
+    [SerializeField]
+    float m_masse = 1;
 
     public void Integrate(float _dt) {
-         transform.position += m_velocity * _dt;
+        m_velocity += m_force / m_masse * _dt;
+        transform.position += m_velocity * _dt;
     }
 
     // Start is called before the first frame update
