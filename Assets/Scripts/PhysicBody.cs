5ad5bfedc94662fd83fa36e696de0cb41c96da74
matthieu-blin <matth.blin@gmail.com>
2021-01-20 16:41:19 +0100

    calcul de la position via la vitesse (integrale approximative)

--- Assets/Scripts/PhysicBody.cs
+++ Assets/Scripts/PhysicBody.cs
@@ -7,9 +7,8 @@ public class PhysicBody : MonoBehaviour
     [SerializeField]
     Vector3 m_velocity = Vector3.zero;
 
-
     public void Integrate(float _dt) {
-
+         transform.position += m_velocity * _dt;
     }
 
     // Start is called before the first frame update
