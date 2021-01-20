b0f651452f3aac44235ec3e7c7b67ff700515e8a
matthieu-blin <matth.blin@gmail.com>
2021-01-20 16:39:32 +0100

    squelette

---
+++ Assets/Scripts/PhysicBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicBody : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 m_velocity = Vector3.zero;
+
+
+    public void Integrate(float _dt) {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhysicManager.Instance.RegisterPhysicBody(this); 
+    }
+}
---
+++ Assets/Scripts/PhysicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicManager : MonoBehaviour
+{
+    public static PhysicManager Instance = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    List<PhysicBody> m_physicBodies = new List<PhysicBody>();
+
+    public void RegisterPhysicBody(PhysicBody _obj)
+    {
+        m_physicBodies.Add(_obj);
+    }
+
+    void FixedUpdate()
+    {
+        foreach(var po in m_physicBodies)
+        {
+            po.Integrate(Time.fixedDeltaTime);
+        }
+    }
+}
