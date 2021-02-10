b39f2d7373b2d19fc34979850862d6b0a309790a
matthieu-blin <matth.blin@gmail.com>
2021-02-10 15:54:17 +0100

    Simple Spring constraint added

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : Constraint
+{
+
+    public Transform m_transform;
+    public float m_stiffness = 0.2f;
+    public float m_dampingCoefficient = 0.4f;
+    public override Vector3 ComputeConstraint()
+    {
+        Vector3  dist = transform.position - m_transform.position;
+        return  -dist * m_stiffness - m_body.Velocity *  m_dampingCoefficient ;
+    }
+
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(m_transform.position, new Vector3(0.2f,0.2f, 0.2f));
+    }
+
+  }
